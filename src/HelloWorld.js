1c4e29f20dee86ec4df5590820e49222267377cd
lecrackdudut <57352312+lecrackdudut@users.noreply.github.com>
2021-03-25 10:02:47 +0100

    composants réutilisables et modulables

---
+++ src/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function HelloWorld(props) {
+    return (
+        <p>Hello {props.name}!</p>
+    );
+}
+
+export default HelloWorld;
