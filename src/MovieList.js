498e394dd28ca5bbb32b73de2a3f83f3b234d516
lecrackdudut <57352312+lecrackdudut@users.noreply.github.com>
2021-03-25 12:21:56 +0100

    movie seach

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Movie from './Movie';
 
 function MovieList(props) {
     const { movies } = props;
+    const [recherche, setRecherche] = useState('');
+
+    function handleChange(event) {
+        setRecherche(event.target.value); 
+    }
 
     return (
         <>
-            {movies.map((movie) => (
-                <Movie movie={movie} />
-            ))}
+            Recherche : <input type="text" value={recherche} onChange={handleChange}/>
+            {movies.map((movie) => {
+                if (movie.name.toUpperCase().includes(recherche.toUpperCase())) {
+                    return (
+                        <Movie movie={movie} />
+                    )
+                }
+                return <></>;
+            })}
         </>
     )
 }
